e1369feafa0a365af481813d4065983ff7153f87
Jan Ferko <julyloov@gmail.com>
2019-06-21 00:10:12 +0200

    Add zomato api env variable

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,9 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :lunchtime,
+  zomato_api_key: System.get_env("ZOMATO_API_KEY")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
